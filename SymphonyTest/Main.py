78ce2e4628b4accccc2552893bfe37d355ef69a2
divyamohan <divyamohan88@gmail.com>
2020-10-01 23:56:33 -0400

    Added modules for Test Data

--- SymphonyTest/Main.py
+++ SymphonyTest/Main.py
@@ -1,29 +1,17 @@
-from selenium import webdriver
-from time import sleep
-import random
 from SymphonyTest.Pages.ForgotPassword import ForgotPassword
 from SymphonyTest.Pages.Login import Login
 from SymphonyTest.Support.errorMessages import errorMessages
 from SymphonyTest.Pages.Authentication import Authentication
 from SymphonyTest.Pages.SignUp import SignUp
 from SymphonyTest.Support.genericSupportModule import visualdelay
-
-
-
-
-def gotoSymphony():
-    chrome_driver = webdriver.Chrome(
-        '/Users/divyamohan/PycharmProjects/pythonProject2/SymphonyTest/driver/chromedriver')
-    chrome_driver.get('https://my.symphony.com/')
-    chrome_driver.maximize_window()
-    return chrome_driver
-
+from SymphonyTest.Support.genericSupportModule import gotoSymphony
+from SymphonyTest.Support.getTestData import getTestData
 
 def test_symphonytest_validlogin_app():
     chrome_driver = gotoSymphony()
     login = Login(chrome_driver)
-    login.enterUsername("divyamohan88@gmail.com")
-    login.enterPassword("Pa55w@rd")
+    login.enterUsername(getTestData.ValidUserName)
+    login.enterPassword(getTestData.ValidPassword)
     login.loginbuttonclick()
     visualdelay()
     auth = Authentication(chrome_driver)
@@ -47,13 +35,11 @@ def test_symphonytest_emptylogin_test():
     chrome_driver.close()
 
 
-
-
 def test_symphonytest_invalidlogin_test():
     chrome_driver = gotoSymphony()
     login = Login(chrome_driver)
-    login.enterUsername("abc123.com")
-    login.enterPassword("abc")
+    login.enterUsername(getTestData.InvalidUserName)
+    login.enterPassword(getTestData.ValidPassword)
     login.loginbuttonclick()
     visualdelay()
     message = login.loginError()
@@ -63,21 +49,15 @@ def test_symphonytest_invalidlogin_test():
     chrome_driver.close()
 
 
-
-
 def test_symphonytest_sign_app():
     chrome_driver = gotoSymphony()
     signUp = SignUp(chrome_driver)
-    signUp.clickonSignUp("abc","abc","divyamohan88@gmail.com","Pa55w@rd")
+    signUp.clickonSignUp(getTestData.FirstName, getTestData.LastName, getTestData.SignUpexisitingEmail, getTestData.Password)
     visualdelay()
-
 
 
 def test_symphonytest_forgot_password():
     chrome_driver = gotoSymphony()
     forgottpassword = ForgotPassword(chrome_driver)
     forgottpassword.clickonForgotPassword()
-    forgottpassword.enterCaptchaInfo("divyamohan88@gmail.com")
-
-
-
+    forgottpassword.enterCaptchaInfo(getTestData.ValidUserName)
--- SymphonyTest/Support/genericSupportModule.py
+++ SymphonyTest/Support/genericSupportModule.py
@@ -1,5 +1,17 @@
 from time import sleep
 import random
+from selenium import webdriver
+import os
+
+
+def gotoSymphony():
+    HOME =os.environ['HOME']
+    chrome_driver = webdriver.Chrome(
+        HOME+'/PycharmProjects/pythonProject2/SymphonyTest/driver/chromedriver')
+
+    chrome_driver.get('https://my.symphony.com/')
+    chrome_driver.maximize_window()
+    return chrome_driver
 
 
 def visualdelay():
